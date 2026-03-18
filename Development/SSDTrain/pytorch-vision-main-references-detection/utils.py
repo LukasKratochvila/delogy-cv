0a886ce9c15436cf0fcf996b8a35b816898d3629
LukasKratochvila <kratochvila.lukas@gmail.com>
2026-03-18 16:05:13 +0100

    Update utils.py
    Unable setup device, change dist backend

--- Development/SSDTrain/pytorch-vision-main-references-detection/utils.py
+++ Development/SSDTrain/pytorch-vision-main-references-detection/utils.py
@@ -272,8 +272,10 @@ def init_distributed_mode(args):
 
     args.distributed = True
 
-    torch.cuda.set_device(args.gpu)
-    args.dist_backend = "nccl"
+    #torch.cuda.set_device(args.gpu)
+    torch.device(args.device)
+    #args.dist_backend = "nccl"
+    args.dist_backend = "gloo"
     print(f"| distributed init (rank {args.rank}): {args.dist_url}", flush=True)
     torch.distributed.init_process_group(
         backend=args.dist_backend, init_method=args.dist_url, world_size=args.world_size, rank=args.rank
